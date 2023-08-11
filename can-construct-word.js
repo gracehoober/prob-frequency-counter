b26b87a948a6ba8580800078b035c230ee8b0b7a
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-08-11 15:35:00 -0700

    can-construct-word completed

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,47 @@
+"use strict";
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+function canConstructWord(word, letters) {
+  const wordFrequency = makeCounter(word);
+  const letterFrequency = makeCounter(letters);
+
+  for(let letter in wordFrequency){
+    if(letterFrequency[letter] === undefined){
+      return false;
+    }
+    if(wordFrequency[letter] > letterFrequency[letter] ){
+      return false;
+    }
+  }
+  return true;
 }
+/** returns an obj of value to frequency */
+function makeCounter(string){
+  const counter = {};
+  for(let char of string){
+    const current = counter[char] || 0;
+    counter[char] = current + 1;
+  }
+  return counter;
+}
+console.log(makeCounter("aa"))
+console.log(makeCounter("abcd"))
+/* Problem solving
+I: two strings
+O: boolean
+assumptions: there are only lowercase letters with not apces or puncs
+
+solve as a human:
+- keep track of all the letters in word and then compare frequency to the occurances in letters
+
+simpler problem
+-ignoring frequency are all the letters in the word in the other string
+
+Pseudo:
+-make a frequency counter
+-input word and letters to the frequency counter to get obj result
+-compare the number of occurance in letters --> must be greater than or equal
+ - if fails return false
+
+-return true
+
+*/
